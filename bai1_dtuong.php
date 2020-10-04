3de13c4c7b00a7c0b9292beb481219c205cd8733
= <lethuytrang183606.com>
2020-10-04 11:56:21 +0700

    bo sung

--- bai1_dtuong.php
+++ bai1_dtuong.php
@@ -7,6 +7,26 @@
             $this->so_thu_2 = $so_thu_2;
         }
 
+        function set_num1($value){
+            $this->so_thu_1 = $value;
+        }
+
+        function set_num2($value){
+            $this->so_thu_2 = $value;
+        }
+
+        function num1(){
+            return $this->so_thu_1;
+        }
+
+        function num2(){
+            return $this->so_thu_2;
+        }
+
+        function ket_qua(){
+            return $ket_qua;
+        }
+        
         function tinh_tong(){
             return $this->so_thu_1 + $this->so_thu_2;
         }
--- dtuong_phep_tinh.php
+++ dtuong_phep_tinh.php
@@ -1,13 +1,4 @@
 
-<?php
-    include('./bai1_dtuong.php');
-    $phep_tinh_1 = New phep_tinh(10, 5);
-    echo '<pre>',print_r($phep_tinh_1),'</pre>';
-    //echo $phep_tinh_1->tinh_tong();
-    //echo $phep_tinh_1->tinh_hieu();
-    //echo $phep_tinh_1->tinh_tich();
-    echo $phep_tinh_1->tinh_chia();
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,11 +24,11 @@
         
             <div class="form-group">
                 <label for="">So thu 1</label>
-                <input type="number" class="form-control" id="" placeholder="Input field" name='so_thu_nhat'>
+                <input type="number" class="form-control" id="" placeholder="Input field" name='so_thu_nhat'  value="<?php if(isset($_POST['so_thu_nhat'])) echo $_POST['so_thu_nhat']?>">
             </div>
             <div class="form-group">
                 <label for="">So thu 2</label>
-                <input type="number" class="form-control" id="" placeholder="Input field" name='so_thu_hai'>
+                <input type="number" class="form-control" id="" placeholder="Input field" name='so_thu_hai' value="<?php if(isset($_POST['so_thu_hai'])) echo $_POST['so_thu_hai']?>">
             </div>
         
 
@@ -46,36 +37,70 @@
                 <div class="col-xs-2"></div>
                 <div class="col-xs-2">
                     <label>
-                        <input type="radio" name="phep_tinh" id="input" value="" checked="checked">
+                        <input type="radio" name="phep_tinh" id="input" value="tong" <?php if($_POST['phep_tinh'] == 'tong') echo 'checked'; ?>>
                         Tổng
                     </label>
                 </div>
                 
                 <div class="col-xs-2">
                     <label>
-                        <input type="radio" name="phep_tinh" id="input" value="" >
+                        <input type="radio" name="phep_tinh" id="input" value="hieu"  <?php if($_POST['phep_tinh'] == 'hieu') echo 'checked'; ?>>
                         Hiệu
                     </label>
                 </div>
                 <div class="col-xs-2">
                     <label>
-                        <input type="radio" name="phep_tinh" id="input" value="" >
+                        <input type="radio" name="phep_tinh" id="input" value="tich" <?php if($_POST['phep_tinh'] == 'tich') echo 'checked'; ?>>
                         Tích
                     </label>
                 </div>
                 <div class="col-xs-2">
                     <label>
-                        <input type="radio" name="phep_tinh" id="input" value="" >
-                        Chia
+                        <input type="radio" name="phep_tinh" id="input" value="thuong" <?php if($_POST['phep_tinh'] == 'thuong') echo 'checked'; ?>>
+                        Thương
                     </label>
                 </div>
                 <div class="col-xs-2"><button type="submit" class="btn btn-primary">Submit</button></div>
             </div>
             
         </form>
-        
+        <legend>KẾT QUẢ</legend>
+        <?php
+            include('./bai1_dtuong.php');
+            if(isset($_POST['so_thu_nhat']) && isset($_POST['so_thu_hai'])){
+                $tinh = New phep_tinh($_POST['so_thu_nhat'], $_POST['so_thu_hai']);
+
+                $string=$tinh->num1().' '.$_POST['phep_tinh'].' '.$tinh->num2()." = ";
+                        
+                        switch ($_POST['phep_tinh']){
+                            case "tong": 
+                                echo $string.$tinh->tinh_tong();
+                            break;
+                            case "hieu":
+                                echo $string.$tinh->tinh_hieu();
+                            break;
+                            case "tich":
+                                echo $string.$tinh->tinh_tich();
+                            break;
+                            case "thuong":
+                                if($_POST['so_thu_hai']!=0){
+                                    echo $string.$tinh->tinh_chia();
+                                }
+                                else {
+                                    echo "Không tính được thương, số thứ hai PHẢI KHÁC 0";
+                                }
+                            break;
+                                        
+                        }
+                }
+    
+   
+    
+        ?>
 
     </div>
+
+    
     
     
 </body>
